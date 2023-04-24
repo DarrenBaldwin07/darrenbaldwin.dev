a469f25c13dabc6ce1e1361a949b408da75cb4dc
Darren B <68653294+Devd0@users.noreply.github.com>
2023-04-24 00:47:31 -0400

    add top language stat

---
+++ app/helpers.ts
@@ -0,0 +1,37 @@
+export const formatNumber = (bounty: string) => {
+	return bounty.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+
+export async function getMostUsedLanguage(username: string): Promise<string> {
+    const headers = {
+      'Authorization': `bearer ${process.env.GITHUB_TOKEN}`,
+    }
+    const res = await fetch(`https://api.github.com/users/${username}/repos`, { method: 'GET', headers: headers })
+
+    const languages: { [key: string]: number } = {};
+
+    const response = await res.json();
+
+    response.forEach((repo: any) => {
+      if (repo.language) {
+        if (languages[repo.language]) {
+          languages[repo.language]++;
+        } else {
+          languages[repo.language] = 1;
+        }
+      }
+    });
+
+    let mostUsedLanguage = "";
+    let mostUsedLanguageCount = 0;
+
+    Object.keys(languages).forEach((language) => {
+      if (languages[language] > mostUsedLanguageCount) {
+        mostUsedLanguage = language;
+        mostUsedLanguageCount = languages[language];
+      }
+    });
+
+    return mostUsedLanguage;
+  }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,10 @@
 import { Heading, Text } from '@rapid-web/ui';
 import { useLoaderData } from "@remix-run/react";
-import { json } from "@remix-run/node";
+import { json } from '@remix-run/node';
+import { formatNumber, getMostUsedLanguage } from '~/helpers';
+import Github from '../../assets/github.svg';
+import Twitter from '../../assets/twitter.svg';
+import Code from '../../assets/terminalSolid.svg';
 
 export async function loader() {
   const headers = {
@@ -43,12 +47,20 @@ export async function loader() {
       }
   }
 
-  return totalContributions
+  let mostUsedLanguage = await getMostUsedLanguage("DarrenBaldwin07");
+
+  const loaderResponse = {
+    totalContributions,
+    mostUsedLanguage
+  }
+
+  return json(loaderResponse, { headers: { "Cache-Control": "public, s-maxage=60" }});
 }
+
+
 
 export default function Index() {
   const data = useLoaderData();
-  console.log(data)
   return (
     <div className='z-10'>
       <Heading styles='text-white text-3xl tracking-widest font-extrabold z-10'>
@@ -57,8 +69,22 @@ export default function Index() {
       <Text className='text-white font-sans mt-4 z-10'>Hey, I’m Darren. I am the Co-founder of <Text as='span' styles='font-bold'>
           <a rel="noreferrer" className='hover:underline decoration-solid' href='https://cincinnati.ventures' target='_blank'>Cincinnati Ventures</a>
         </Text>, a venture backed company building the startups of tomorrow.</Text>
+        <div className='flex flex-col mt-8 space-y-6'>
+          <div className='flex space-x-2 items-center'>
+            <img width={26} src={Github} alt='github' />
+            <Text styles='text-white'>{formatNumber(data.totalContributions.toString())} contributions in the last year</Text>
+          </div>
+          <div className='flex space-x-2 items-center'>
+            <img width={26} src={Twitter} alt='twitter' />
+            <Text styles='text-white'>{400} all time tweets</Text>
+          </div>
 
-        <Text styles='text-white font-sans'>I contribute and maintain various open source projects and am very passionate about fullstack web development and performance with tools like <strong>Typescript</strong> and <strong>Rust</strong>. I’m also an advisor and future Angel Investor in early stage startups</Text>
+          <div className='flex space-x-2 items-center'>
+            <img width={26} src={Code} alt='code' />
+            <Text styles='text-white'>Most used language is: {data.mostUsedLanguage}</Text>
+          </div>
+        </div>
+        <Text styles='text-white font-sans mt-8'>I contribute and maintain various open source projects and am very passionate about fullstack web development and performance with tools like <strong>Typescript</strong> and <strong>Rust</strong>. I’m also an advisor and future Angel Investor in early stage startups</Text>
     </div>
   );
 }
