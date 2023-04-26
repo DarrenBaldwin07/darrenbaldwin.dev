63ba3d3ad1dd437434b1999e7ca9149e99e9d9a1
Darren B <68653294+Devd0@users.noreply.github.com>
2023-04-26 00:03:04 -0400

    fix layout

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -17,28 +17,31 @@ const Layout: React.FC<Props> = ({ children }: Props) => {
 
 	return (
 		<div className='overflow-hidden relative'>
-			<Tooltip
-				styles='bg-background border-backgroundSecondary'
-				label={isStarred ? '' : 'Star this portfolio!'}
-				orientation='bottom'
-			>
-				<div
-					onClick={() => setIsStarred(!isStarred)}
-					className='bg-background border z-10 border-backgroundSecondary hover:border-[#4C5762] transition ease-linear p-2 rounded-xl flex items-center justify-center hover:cursor-pointer w-max absolute right-[40px] top-[40px]'
+			<Container maxWidth='md' styles='z-10 flex justify-end'>
+				<Tooltip
+					styles='bg-background border-backgroundSecondary'
+					label={isStarred ? '' : 'Star this portfolio!'}
+					orientation='bottom'
 				>
-					<img
-						src={isStarred ? Star : UnStar}
-						alt='un-star'
-						width={22}
-					/>
-					<Text styles='text-white ml-2 mr-6'>
-						{isStarred ? 'Starred' : 'Star'}
-					</Text>
-					<div className='bg-backgroundSecondary p-1 rounded-full w-6 h-6 text-center flex items-center justify-center'>
-						<Text styles='text-white text-sm'>0</Text>
+					<div
+						onClick={() => setIsStarred(!isStarred)}
+						className='bg-background border z-10 border-backgroundSecondary hover:border-[#4C5762] transition ease-linear p-2 rounded-xl flex items-center relative right-0 justify-self-end hover:cursor-pointer w-max top-[40px]'
+					>
+						<img
+							src={isStarred ? Star : UnStar}
+							alt='un-star'
+							width={22}
+						/>
+						<Text styles='text-white ml-2 mr-6'>
+							{isStarred ? 'Starred' : 'Star'}
+						</Text>
+						<div className='bg-backgroundSecondary p-1 rounded-full w-6 h-6 text-center flex items-center justify-center'>
+							<Text styles='text-white text-sm'>0</Text>
+						</div>
 					</div>
-				</div>
-			</Tooltip>
+				</Tooltip>
+			</Container>
+
 			<img
 				src={gradient1}
 				alt='gradient1'
@@ -54,7 +57,7 @@ const Layout: React.FC<Props> = ({ children }: Props) => {
 				alt='gradient3'
 				className='absolute -top-[200px] lg:-top-[850px] sm:-top-[300px] md:-top-[400px] left-1/2 -translate-x-1/2 overflow-hidden'
 			/>
-			<Container maxWidth='sm' styles='z-10 pb-12'>
+			<Container maxWidth='sm' styles='z-10 pb-12 h-screen'>
 				<div className='mt-36 flex z-10'>
 					<div className='flex flex-col items-center mr-32'>
 						<img
