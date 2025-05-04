a25680b0d745ad0ff889e244f17f94815da2410a
Darren B <68653294+Devd0@users.noreply.github.com>
2025-05-04 14:25:26 -0400

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
 	return (
 		<div className=''>
-			<h1>darrenbaldwin.dev</h1>
+			<div className='flex flex-col'>
+				<h1 className='text-white font-medium text-md'>Darren Baldwin</h1>
+				<h3 className='text-gray-400 font-medium text-md'>
+					Founder & Product Builder
+				</h3>
+			</div>
 		</div>
 	);
 }
